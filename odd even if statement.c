8a0a6f280114b4de2f7c6f516bc917d01cef8aef
pullamsettymaanasa <130460207+pullamsettymaanasa@users.noreply.github.com>
2023-04-11 18:35:08 +0530

    Add files via upload

---
+++ odd even if statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int num;
+ printf(“Enter a number:”);
+ if(num%2==0)
+ {
+printf("%d is even.”);
+ }
+ else
+ {
+printf("%d is Odd”);
+ }
+ return 0;
+}
+
